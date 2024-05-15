e864823f41ce97ece906ae91a6acd2663570dc0f
Biasiolo <biasiolorafael@gmail.com>
2024-05-15 10:50:40 -0300

    initial

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Array de objetos com nome e nota de 5 alunos
 var alunos = [{
   nome: 'João',
   nota: 7
@@ -17,17 +16,12 @@ var alunos = [{
   nome: 'Lucas',
   nota: 6
 }];
-
-// Função para retornar alunos com nota maior ou igual a 6
+add;
 var filtraAlunosAprovados = function filtraAlunosAprovados(alunos) {
   return alunos.filter(function (_ref) {
     var nota = _ref.nota;
     return nota >= 6;
   });
 };
-
-// Usando a função
 var alunosAprovados = filtraAlunosAprovados(alunos);
-
-// Exibindo o resultado
 console.log(alunosAprovados);
